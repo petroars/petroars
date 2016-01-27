bdaee5cba9650cc6bde31b4642297d47c701a911
Leonid <web@mianofv.ru>
2016-01-27 22:39:18 +0100

    1

--- mailer.php
+++ mailer.php
@@ -4,7 +4,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
  
-$to = 'web@mianofv.ru';
+$to = 'sale@petroars.cz';
 $subject = 'the subject';
 $message = 'FROM: '.$name.' EMAIL: '.$email.' MESSAGE: '.$message;
 $headers = 'From: '. $email. "\r\n";
